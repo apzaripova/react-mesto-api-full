7892bd2070fa0af88e87dd76fb893901300b04df
apzaripova <ap.zaripova@gmail.com>
2021-12-16 15:36:15 +0300

    fix login

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,17 +1,19 @@
 const jwt = require('jsonwebtoken');
 const NotAuthError = require('../errors/NotAuthError');
 
-const auth = (req, res, next) => {
-  const { cookie } = req.headers;
+const { NODE_ENV, JWT_SECRET } = process.env;
 
-  if (!cookie || !cookie.startsWith('jwt=')) {
+const auth = (req, res, next) => {
+  const { authorization } = req.headers;
+
+  if (!authorization || !authorization.startsWith('Bearer ')) {
     throw new NotAuthError('Необходима авторизация');
   }
-  const token = cookie.replace('jwt=', '');
+  const token = authorization.replace('Bearer ', '');
   let payload;
 
   try {
-    payload = jwt.verify(token, 'dev-secret-key');
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
     throw new NotAuthError('Необходима авторизация');
   }
