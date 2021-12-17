24a611adbbd25fcdbfe54d3ec531d3cdb23035b6
apzaripova <ap.zaripova@gmail.com>
2021-12-17 23:43:37 +0300

    add cors

--- backend/app.js
+++ backend/app.js
@@ -34,7 +34,19 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useFindAndModify: false,
 });
 
-app.use(cors());
+const options = {
+  origin: [
+    'http;//localhost:3000',
+    'https://domainname.mesto.nomoredomains.rocks',
+    'http://domainname.mesto.nomoredomains.rocks'],
+  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+  allowedHeaders: ['Content-Type', 'origin', 'Authorization'],
+  credentials: true,
+};
+
+app.use('*', cors(options));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
