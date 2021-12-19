319fb8045bad372075cccdce5fdbc75ea695196f
apzaripova <ap.zaripova@gmail.com>
2021-12-20 01:09:33 +0300

    proverka

--- backend/models/user.js
+++ backend/models/user.js
@@ -7,14 +7,12 @@ const NotAuthError = require('../errors/NotAuthError');
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true,
     minlength: 2,
     maxlength: 30,
     default: 'Жак-Ив Кусто',
   },
   about: {
     type: String,
-    required: true,
     minlength: 2,
     maxlength: 30,
     default: 'Исследователь',
